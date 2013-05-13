0b48bc39be33f56c1e1d5b3f30cbb697c3c335dd
Sirvan Almasi Laptop Computer <sirvan3tr@gmail.com>
2013-05-13 22:29:56 +0100

    latest chagnes

--- index.php
+++ index.php
@@ -46,8 +46,6 @@ require_once('config.php');
   </div>
 </div>
 
-
-<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=WEEK&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=salmasi%40contentformula.com&amp;color=%232F6309&amp;ctz=Europe%2FLondon" style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no"></iframe>
           <form class="form-inline">
             <?php
             $filterusers = ORM::for_table('users')->select('name')->find_many();
--- skill_hours_report.php
+++ skill_hours_report.php
@@ -26,18 +26,19 @@
       include("config.php");
       echo '<th>';
   for ($i = 0; $i <= 11; $i++) {
-        echo '<td class="content">'.date("F",strtotime($i."Months")).'</td>';
+      echo '<td class="content">'.date("F",strtotime($i."Months")).'</td>';
   }
       echo '</th>';
 
       echo "</thead><tbody>";
       $query = ORM::for_table('skill_type')->find_many();
-
-        foreach ($query as $query) { 
+	  
+      foreach ($query as $query) { 
         echo "<tr>";
         echo '<td ><span class="matrixtaskname fl"><span data-toggle="tooltip" title="" data-original-title="' .$query->skill_full. '" class="matrixhours" style="background-color:'.$query->color.';">'.$query->skill. '</span> '.$query->skill_full.'</td>';
         $skilltypeid = $query->id;
-
+		
+		$overalltotalhrs = 0;
 for ($i = 0; $i <= 11; $i++) {
         echo '<td class="content">';
         $date = date("Y-m-d",strtotime($i."Months"));
@@ -46,31 +47,32 @@ for ($i = 0; $i <= 11; $i++) {
     $firstdate = date('Y-m-d', mktime(0, 0, 0, $month, 1, $year));
     $lastdate = date('Y-m-t', mktime(0, 0, 0, $month, 1, $year));
 
-    $queryhours = mysqli_query($con, "SELECT * FROM task_repetition WHERE skill_type_id='$skilltypeid' and date >= '$firstdate' and date <= '$lastdate' ");
-    $queryhours = ORM::for_table('skill_type')->find_many();
-    $querybulkhours = mysqli_query($con, "SELECT * FROM skill_hrs_bulk_addition WHERE skill_type_id='$skilltypeid' and date >= '$firstdate' and date <= '$lastdate' ");
-    $querybulkhours = ORM::for_table('skill_type')->find_many();
+    $queryhours = ORM::for_table('task_repetition')->where('skill_type_id', $skilltypeid)->where_gte('date', $firstdate)->where_lte('date', $lastdate)->find_many();
+    $querybulkhours = ORM::for_table('task_repetition')->where('skill_type_id', $skilltypeid)->where_gte('date', $firstdate)->where_lte('date', $lastdate)->find_many();
 
     $querytotalhrs = 0;
-    foreach ($querytotalhrs as $querytotalhrs) {
-        $querytotalhrs = $querytotalhrs + $rowhours['duration'];
-        
+    foreach ($queryhours as $queryhours) {
+        $querytotalhrs = $querytotalhrs + $queryhours->duration;
       }
       $querybulktotalhrs = 0;
     foreach ($querybulkhours as $querybulkhours) {
-      {
-        $querybulktotalhrs = $querybulktotalhrs + $rowbulkhours['hrs'];
-        
+        $querybulktotalhrs = $querybulktotalhrs + $querybulkhours->hrs;
       }
       $totalhrs = $querytotalhrs + $querybulktotalhrs;
       echo '<div class="hours-div" querytotalhrs="'.$querytotalhrs.'" querybulktotalhrs="'.$querybulktotalhrs.'" date="'.$firstdate.'" skilltypeid="'.$skilltypeid.'" data-toggle="tooltip" data-original-title="Click here to add bulk hours for this skill type">'.$totalhrs.'</div>';
      
       echo "</td>";
-
+		$overalltotalhrs = $overalltotalhrs + $totalhrs;
 }
         echo "</tr>";
         }
      ?>
+	 <tr>
+		 <td><b>Total hours:</b></td>
+		 <td>
+			<?php echo $overalltotalhrs; ?>
+		 </td>
+	 </tr>
     </table>
     </div><!-- /span12 -->
   </div>
